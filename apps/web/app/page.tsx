f56e41f828300ca11f5b22a887da6fe0fe9a462f
talal alghazal <talalalghazal55@gmail.com>
2026-07-10 18:00:40 +0300

    fix: change the glow effect on hover for the image in the vision section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -146,11 +146,11 @@ export default function Page() {
       >
         <h1 className="text-center font-thmanyah-heading text-5xl">رؤيتنا</h1>
         <div className="flex items-center justify-between gap-8">
-          <p className="max-w-lg font-thmanyah-heading text-2xl leading-loose">
-            نحن نؤمن بأن التسويق الرقمي هو مفتاح النجاح في العصر الحديث، ونعمل
+          <p className="max-w-lg font-thmanyah-heading not-lg:text-center text-2xl leading-loose">
+            نؤمن بأن التسويق الرقمي هو مفتاح النجاح في العصر الحديث، ونعمل
             على تطوير حلول تسويقية مبتكرة تساعد الشركات على النمو والتطور.
           </p>
-          <div className="ease absolute -left-40 z-0 h-100 w-[50%] scale-100 rotate-4 rounded-2xl border border-primary/60 bg-accent/30 p-4 shadow-2xl shadow-black/50 duration-200 not-lg:hidden hover:border-primary/80 hover:shadow-primary/60">
+          <div className="ease absolute -left-40 z-0 h-100 w-[50%] scale-100 rotate-4 rounded-2xl border border-muted-foreground/70 bg-accent/30 p-4 shadow-2xl shadow-black/50 duration-200 not-lg:hidden hover:border-primary hover:shadow-primary/60">
             <div className="relative h-full w-full">
               <Image
                 src={"/images/Marketing2.jpg"}
