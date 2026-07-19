93d2eb16f5060ca5ea40d10f40c481746b826c48
talal alghazal <talalalghazal55@gmail.com>
2026-07-19 09:11:04 +0300

    add animations to the hero section components

--- apps/web/components/sections/hero-section.tsx
+++ apps/web/components/sections/hero-section.tsx
@@ -11,47 +11,49 @@ export default function HeroSection() {
   return (
     <SectionTemplate className="min-h-svh gap-4 sm:items-center md:flex-row md:items-center md:justify-between md:gap-6">
       <div className="flex flex-col gap-5 not-md:text-center">
-        <SectionHeading
-          title="علامة تجاريّة تليقُ بمشروعِك"
-          subtitle="نساعدك على بناء هوية بصرية قوية تُعبّر عن علامتك التجارية وتميّزها في السوق."
-          align="start"
-          className="gap-5"
-        />
+        <motion.div transition={{ delay: 1, duration: 1.2 }}>
+          <SectionHeading
+            title="علامة تجاريّة تليقُ بمشروعِك"
+            subtitle="نساعدك على بناء هوية بصرية قوية تُعبّر عن علامتك التجارية وتميّزها في السوق."
+            align="start"
+            className="gap-5"
+          />
+        </motion.div>
         <div className="flex items-center gap-3 not-md:justify-center">
-          <Button className="w-fit font-thmanyah-subheading-sans" size="lg">
-            <HugeiconsIcon
-              icon={Rocket01Icon}
-              className="size-5"
-              strokeWidth={2}
-            />
-            اطلب استشارة
-          </Button>
-          <Button
-            variant="secondary"
-            className="w-fit border border-primary/60 bg-accent font-thmanyah-subheading-sans hover:bg-accent/80"
-            size="lg"
-          >
-            <HugeiconsIcon
-              icon={ArrowDown02Icon}
-              className="size-5"
-              strokeWidth={2}
-            />
-            تعرف على خدماتنا
-          </Button>
+          <motion.div whileHover={{ scale: 1.1 }}>
+            <Button className="w-fit font-thmanyah-subheading-sans" size="lg">
+              <HugeiconsIcon
+                icon={Rocket01Icon}
+                className="size-5"
+                strokeWidth={2}
+              />
+              اطلب استشارة
+            </Button>
+          </motion.div>
+          <motion.div whileHover={{scale: 1.1}}>
+            <Button
+              variant="secondary"
+              className="w-fit border border-primary/60 bg-accent font-thmanyah-subheading-sans hover:bg-accent/80"
+              size="lg"
+            >
+              <HugeiconsIcon
+                icon={ArrowDown02Icon}
+                className="size-5"
+                strokeWidth={2}
+              />
+              تعرف على خدماتنا
+            </Button>
+          </motion.div>
         </div>
       </div>
       <motion.div
-        initial={{ translateX: "-1000px", opacity: 0, }}
-        animate={{ translateX: "0", opacity: 1 }}
-        transition={{ bounce: 10, duration: 1, ease: "backOut" }}
+        transition={{ duration: 1.2 }}
         whileHover={{
           border: "1px solid var(--color-primary)",
-          filter: "brightness(1.1)",
-          boxShadow:
-            "0 0 20px -5px var(--color-primary), 0 0 40px -10px var(--color-primary), 0 25px 50px -12px var(--color-primary, rgb(0 0 0 / 0.3))",
-          transition: { type: "tween", duration: 0.8, ease: "easeInOut" },
+          rotate: "-2deg",
+          transition: { type: "tween", duration: 0.3, ease: "easeInOut" },
         }}
-        className="ease h-100 w-[50%] rounded-2xl border border-primary/60 bg-accent/30 p-4 shadow-2xl shadow-black/50 not-md:hidden"
+        className="ease h-100 w-[50%] rounded-2xl border border-primary/60 bg-accent/30 p-4 shadow-2xl shadow-black/50 transition-shadow duration-400 not-md:hidden hover:shadow-2xl! hover:shadow-primary/60!"
       >
         <div className="relative h-full w-full">
           <Image
